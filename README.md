3b800e8580cacbf7213296e317c56624644a36e0
Omeiza Owuda <owudaomeiza@gmail.com>
2023-05-20 10:45:12 +0100

    Project euler problem 10 solution

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# summation-of-primes
+# Project Euler Problem 10
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+
+[Project euler problem 10](https://projecteuler.net/problem=10)
---
+++ index.ts
@@ -0,0 +1,24 @@
+/**
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ *
+ * Find the sum of all the primes below two million.
+ */
+
+function sumOfPrimes(limit: number): number {
+	function isPrime(input: number): boolean {
+		for (let i = 2; i < input; i++) {
+			if (input % i === 0) return false;
+		}
+
+		return input > 1;
+	}
+
+	let primeSum = 0;
+	for (let i = 0; i < limit; i++) {
+		if (isPrime(i)) primeSum = primeSum + i;
+	}
+
+	return primeSum;
+}
+
+console.log(sumOfPrimes(2000000));
